b2f4c004c74682a2c704e679eb4a719e734fb938
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2021-10-31 00:36:39 -0300

    Refatoração para arquitetura Elm

--- src/paginas/ProcessoSelecionar.tsx
+++ src/paginas/ProcessoSelecionar.tsx
@@ -1,10 +1,30 @@
 import { JSXInternal } from 'preact/src/jsx';
+import { createBroadcastService } from '../createBroadcastService';
+import { getBloco, getBlocos, updateBloco } from '../database';
 import { expectUnreachable } from '../lib/expectUnreachable';
 import { Handler } from '../lib/Handler';
-import { assert, isNotNull, NonNegativeInteger } from '../lib/predicates';
-import { ClientAction, ClientMessage, isResponseMessage, RequestMessage } from '../types/Action';
-import { BlocoProcesso } from '../types/Bloco';
-import { isNumProc, NumProc } from '../types/NumProc';
+import { assert, isNotNull } from '../lib/predicates';
+import { BroadcastMessage } from '../types/Action';
+import { Bloco, BlocoProcesso } from '../types/Bloco';
+import { NumProc } from '../types/NumProc';
+
+type Model =
+  | { status: 'Loading' }
+  | { status: 'Success'; blocos: Bloco[] }
+  | { status: 'Error'; reason: unknown };
+type Lazy<T> = { (): T };
+type Cmd<T> = Handler<Handler<T>>;
+
+const Action = {
+  Blocos: (blocos: Bloco[]) => ({ type: 'Blocos' as const, blocos }),
+  Error: (reason: unknown) => ({ type: 'Error' as const, reason }),
+  Inserir: (bloco: Bloco['id']) => ({ type: 'Inserir' as const, bloco }),
+  InserirEFechar: (bloco: Bloco['id']) => ({ type: 'InserirEFechar' as const, bloco }),
+  NoOp: () => ({ type: 'NoOp' as const }),
+  ObterBlocos: () => ({ type: 'ObterBlocos' as const }),
+  Remover: (bloco: Bloco['id']) => ({ type: 'Remover' as const, bloco }),
+};
+type Action = ReturnType<typeof Action[keyof typeof Action]>;
 
 const css = /*css*/ `
 div#gm-blocos {
@@ -14,7 +34,6 @@ div#gm-blocos {
 `;
 
 export function ProcessoSelecionar(numproc: NumProc) {
-  assert(window.opener instanceof Window);
   const mainMenu = document.getElementById('main-menu');
   assert(isNotNull(mainMenu));
   document.head.appendChild(
@@ -25,112 +44,168 @@ export function ProcessoSelecionar(numproc: NumProc) {
   );
   const div = document.createElement('div');
   mainMenu.insertAdjacentElement('beforebegin', div);
-  preact.render(<Main numproc={numproc} opener={window.opener} />, mainMenu, div);
+  preact.render(<Main numproc={numproc} />, mainMenu, div);
 }
 
-function useClientReducer(
-  opener: Window,
-  numproc: NumProc,
-): [BlocoProcesso[], Handler<ClientAction>] {
-  const [blocos, setBlocos] = preactHooks.useState<BlocoProcesso[]>([]);
+function mount(
+  init: Lazy<[Model] | [Model, Cmd<Action>]>,
+  createUpdate: (
+    send: Handler<BroadcastMessage>,
+  ) => (state: Model, action: Action) => [Model] | [Model, Cmd<Action>],
+): [Model, Handler<Action>] {
+  const [initialState, initialCommand] = preactHooks.useMemo(init, []);
 
-  const [ids, setIds] = preactHooks.useState(new Set<NonNegativeInteger>());
+  const [state, setState] = preactHooks.useState(initialState);
 
-  const getRequestId = preactHooks.useCallback(() => {
-    const last = Math.max(-1, ...ids);
-    const next = (last + 1) as NonNegativeInteger;
-    setIds(s => s.add(next));
-    return next;
-  }, [ids]);
+  const dispatch = preactHooks.useCallback(handler, [state]);
 
-  preactHooks.useEffect(() => {
-    send({ type: 'ObterBlocosProcesso', numproc });
-    window.addEventListener('message', listener);
-    return () => window.removeEventListener('message', listener);
-
-    function listener(evt: MessageEvent<unknown>) {
-      try {
-        assert(evt.origin === document.location.origin, 'o');
-        assert(evt.source === opener, 's');
-        assert(isResponseMessage(evt.data), `Mensagem desconhecida: ${JSON.stringify(evt.data)}.`);
-        const { message, responseId } = evt.data;
-        assert(ids.has(responseId));
-        ids.delete(responseId);
-        switch (message.type) {
-          case 'FornecerBlocosProcesso': {
-            setBlocos(message.blocos);
-            break;
-          }
-
-          // default:
-          //   expectUnreachable(message);
-        }
-      } catch (_) {
-        console.error(_);
+  const update = preactHooks.useMemo(() => {
+    const bc = createBroadcastService();
+    bc.subscribe(action => {
+      switch (action.type) {
+        case 'Blocos':
+          dispatch(action);
       }
-    }
+    });
+    return createUpdate(bc.publish);
   }, []);
 
-  return [blocos, dispatch];
+  preactHooks.useEffect(() => {
+    if (initialCommand) go(initialState, initialCommand)(setState);
+  }, []);
 
-  function dispatch(action: ClientAction) {
-    switch (action.type) {
-      case 'InserirEmBloco': {
-        if (blocos.some(x => x.id === action.bloco && !x.inserido)) {
-          setBlocos([]);
-          send({ type: 'InserirProcesso', bloco: action.bloco, numproc });
-        }
-        break;
-      }
-      case 'RemoverDeBloco': {
-        if (blocos.some(x => x.id === action.bloco && x.inserido)) {
-          setBlocos([]);
-          send({ type: 'RemoverProcesso', bloco: action.bloco, numproc });
-        }
-        break;
-      }
-      default:
-        expectUnreachable(action);
-    }
+  return [state, dispatch];
+
+  function go(...args: [Model] | [Model, Cmd<Action>]): Cmd<Model> {
+    const [state, cmd] = args;
+    if (!cmd) return f => f(state);
+    return f => cmd(action => go(...update(state, action))(f));
   }
 
-  function send(message: ClientMessage) {
-    const requestId = getRequestId();
-    const request: RequestMessage = { requestId, message };
-    opener.postMessage(request, document.location.origin);
+  function handler(action: Action) {
+    go(...update(state, action))(setState);
   }
 }
 
-function Main(props: { numproc: NumProc; opener: Window }) {
-  const [blocos, dispatch] = useClientReducer(props.opener, props.numproc);
+function createUpdate(numproc: NumProc) {
+  return (send: Handler<BroadcastMessage>) => {
+    return (state: Model, action: Action): [Model] | [Model, Cmd<Action>] => {
+      switch (action.type) {
+        case 'Blocos':
+          return [{ status: 'Success', blocos: action.blocos }];
+
+        case 'Error':
+          return [{ status: 'Error', reason: action.reason }];
+
+        case 'Inserir':
+        case 'InserirEFechar':
+          return [
+            { status: 'Loading' },
+            resolve =>
+              Promise.resolve()
+                .then(async () => {
+                  const bloco = await getBloco(action.bloco);
+                  if (!bloco) return;
+                  const processos = new Set(bloco.processos).add(numproc);
+                  await updateBloco({ ...bloco, processos: [...processos] });
+                  return;
+                })
+                .then(
+                  async () => {
+                    if (action.type === 'InserirEFechar') {
+                      send(Action.Blocos(await getBlocos()));
+                      window.close();
+                    } else {
+                      resolve(Action.ObterBlocos());
+                    }
+                  },
+                  reason => {
+                    resolve(Action.Error(reason));
+                  },
+                ),
+          ];
+
+        case 'NoOp':
+          return [state];
+
+        case 'ObterBlocos':
+          return [
+            { status: 'Loading' },
+            resolve => {
+              getBlocos()
+                .then(Action.Blocos)
+                .then(action => {
+                  send(action);
+                  return action;
+                })
+                .catch(Action.Error)
+                .then(resolve);
+            },
+          ];
+
+        case 'Remover':
+          return [
+            { status: 'Loading' },
+            resolve =>
+              Promise.resolve()
+                .then(async () => {
+                  const bloco = await getBloco(action.bloco);
+                  if (!bloco) return Action.ObterBlocos();
+                  const processos = new Set(bloco.processos);
+                  processos.delete(numproc);
+                  await updateBloco({ ...bloco, processos: [...processos] });
+                  return Action.ObterBlocos();
+                })
+                .catch(Action.Error)
+                .then(resolve),
+          ];
+      }
+      return expectUnreachable(action);
+    };
+  };
+}
+
+function Main(props: { numproc: NumProc }) {
+  const [state, dispatch] = mount(
+    () => [{ status: 'Loading' }, resolve => resolve(Action.ObterBlocos())],
+    createUpdate(props.numproc),
+  );
 
   return (
     <div id="gm-blocos">
-      {blocos.length > 0 ? <Blocos blocos={blocos} dispatch={dispatch} /> : null}
+      {state.status === 'Success' ? (
+        <Blocos
+          blocos={state.blocos.map(({ processos, ...rest }) => ({
+            ...rest,
+            inserido: processos.includes(props.numproc),
+          }))}
+          dispatch={dispatch}
+        />
+      ) : null}
     </div>
   );
 }
 
-function Blocos(props: { blocos: BlocoProcesso[]; dispatch: Handler<ClientAction> }) {
+function Blocos(props: { blocos: BlocoProcesso[]; dispatch: Handler<Action> }) {
   return (
     <>
       <h2>Blocos</h2>
       <ul>
         {props.blocos.map(info => (
-          <Bloco {...info} dispatch={props.dispatch} />
+          <Bloco key={info.id} {...info} dispatch={props.dispatch} />
         ))}
       </ul>
     </>
   );
 }
 
-function Bloco(props: BlocoProcesso & { dispatch: Handler<ClientAction> }) {
+function Bloco(props: BlocoProcesso & { dispatch: Handler<Action> }) {
   const onChange = preactHooks.useCallback(
     (evt: JSXInternal.TargetedEvent<HTMLInputElement>) => {
       if (evt.currentTarget.checked) {
-        props.dispatch({ type: 'InserirEmBloco', bloco: props.id });
+        props.dispatch(Action.Inserir(props.id));
       } else {
-        props.dispatch({ type: 'RemoverDeBloco', bloco: props.id });
+        props.dispatch(Action.Remover(props.id));
       }
     },
     [props.dispatch],
@@ -138,9 +213,15 @@ function Bloco(props: BlocoProcesso & { dispatch: Handler<ClientAction> }) {
   return (
     <li>
       <label>
-        <input type="checkbox" checked={props.inserido} onChange={onChange} />
-        {props.nome} ({props.id})
-      </label>
+        <input type="checkbox" checked={props.inserido} onChange={onChange} /> {props.nome}
+      </label>{' '}
+      {props.inserido ? null : (
+        <input
+          type="image"
+          src="infra_css/imagens/transportar.gif"
+          onClick={() => props.dispatch(Action.InserirEFechar(props.id))}
+        />
+      )}
     </li>
   );
 }
