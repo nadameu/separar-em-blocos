75785579068e2f75abb8a323b41d85b5b4f29d91
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2021-11-19 23:28:54 -0300

    Removida a dependência "idb"

--- src/database.ts
+++ src/database.ts
@@ -1,45 +1,72 @@
-import { DBSchema, deleteDB, openDB } from 'idb';
-import { assert, NonEmptyString, NonNegativeInteger } from './lib/predicates';
-import { Bloco, isBloco } from './types/Bloco';
+import { assert, isAnyOf, isUndefined, NonEmptyString, NonNegativeInteger } from './lib/predicates';
+import { Bloco, isBloco, isIdBloco } from './types/Bloco';
 
-interface Dados extends DBSchema {
-  blocos: { key: Bloco['id']; value: Bloco; indexes: { nome: 'nome'; processos: 'processos' } };
+function promisifyRequest<T>(req: IDBRequest<T>): Promise<T> {
+  return new Promise((res, rej) => {
+    req.addEventListener('error', onError);
+    req.addEventListener('success', onSuccess);
+
+    function onError() {
+      rej(req.error);
+      removeEventListeners();
+    }
+
+    function onSuccess() {
+      res(req.result);
+      removeEventListeners();
+    }
+
+    function removeEventListeners() {
+      req.addEventListener('error', onError);
+      req.addEventListener('success', onSuccess);
+    }
+  });
 }
 
 export function open() {
-  return openDB<Dados>('gm-blocos', 4, {
-    upgrade(db, oldVersion, newVersion, transaction) {
-      if (oldVersion < 1) {
-        db.createObjectStore('blocos', { keyPath: 'id' });
-      }
-      if (oldVersion < 2) {
-        transaction.objectStore('blocos').createIndex('nome', ['nome'], { unique: true });
-      }
-      if (oldVersion < 3) {
-        const blocos = transaction.objectStore('blocos');
-        blocos.deleteIndex('nome');
-        blocos.createIndex('nome', 'nome', { unique: true });
-      }
-      if (oldVersion < 4) {
-        const blocos = transaction.objectStore('blocos');
-        blocos.createIndex('processos', 'processos', { multiEntry: true });
-      }
-    },
-    blocked() {},
-    blocking() {},
-    terminated() {},
+  const req = indexedDB.open('gm-blocos', 4);
+  req.addEventListener('upgradeneeded', onUpgradeNeeded);
+
+  return promisifyRequest(req).finally(() => {
+    req.removeEventListener('upgradeneeded', onUpgradeNeeded);
   });
+
+  function onUpgradeNeeded({ oldVersion }: IDBVersionChangeEvent) {
+    const db = req.result;
+    const transaction = req.transaction!;
+    let store: IDBObjectStore;
+    if (oldVersion < 1) {
+      store = db.createObjectStore('blocos', { keyPath: 'id' });
+    } else {
+      store = transaction.objectStore('blocos');
+    }
+    if (oldVersion < 2) {
+      store.createIndex('nome', ['nome'], { unique: true });
+    }
+    if (oldVersion < 3) {
+      store.deleteIndex('nome');
+      store.createIndex('nome', 'nome', { unique: true });
+    }
+    if (oldVersion < 4) {
+      store.createIndex('processos', 'processos', { multiEntry: true });
+    }
+  }
+}
+
+async function makeRequest<T>(
+  mode: 'readonly' | 'readwrite',
+  createRequest: (store: IDBObjectStore) => IDBRequest<T>,
+): Promise<T> {
+  const db = await open();
+  return promisifyRequest(createRequest(db.transaction('blocos', mode).objectStore('blocos')));
 }
 
 export function deleteBlocos() {
-  return deleteDB('gm-blocos', {
-    blocked() {},
-  });
+  return promisifyRequest(indexedDB.deleteDatabase('gm-blocos'));
 }
 
 export async function getBlocos() {
-  const db = await open();
-  const blocos = await db.getAll('blocos');
+  const blocos = await makeRequest('readonly', (store) => store.getAll());
   assert(blocos.every(isBloco), 'Formato do banco de dados desconhecido.');
   return blocos.sort((a, b) => {
     const [c, d] = [a, b].map((x) => x.nome) as [NonEmptyString, NonEmptyString];
@@ -53,21 +80,25 @@ export async function getBlocos() {
 }
 
 export async function getBloco(id: NonNegativeInteger) {
-  const db = await open();
-  return db.get('blocos', id);
+  const bloco = await makeRequest('readonly', (store) => store.get(id));
+  assert(isAnyOf(isBloco, isUndefined)(bloco));
+  return bloco;
 }
 
-export async function createBloco(bloco: Bloco) {
-  const db = await open();
-  return db.add('blocos', bloco);
+export function createBloco(bloco: Bloco) {
+  return writeBloco('add', bloco);
 }
 
 export async function deleteBloco(id: Bloco['id']) {
-  const db = await open();
-  await db.delete('blocos', id);
+  await makeRequest('readwrite', (store) => store.delete(id));
 }
 
-export async function updateBloco(bloco: Bloco) {
-  const db = await open();
-  await db.put('blocos', bloco);
+export function updateBloco(bloco: Bloco) {
+  return writeBloco('put', bloco);
+}
+
+async function writeBloco(method: 'add' | 'put', bloco: Bloco) {
+  const result = await makeRequest('readwrite', (store) => store[method](bloco));
+  assert(isIdBloco(result));
+  return result;
 }
